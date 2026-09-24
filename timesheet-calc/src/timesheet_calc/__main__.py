acc32e9ef47a85f14b72e09ed24a7064fddde328
CarlitoGR <36905347+CarlitoGR@users.noreply.github.com>
2026-09-24 13:52:09 -0400

    Delete timesheet-calc directory

--- timesheet-calc/src/timesheet_calc/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Allow ``python -m timesheet_calc``."""
-
-from timesheet_calc.cli import main
-
-raise SystemExit(main())
--- timesheet-calc/src/timesheet_calc/web/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Web front end and JSON API. Requires the ``web`` extra: ``pip install timesheet-calc[web]``."""
